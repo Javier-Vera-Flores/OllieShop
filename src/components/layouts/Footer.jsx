bf88433ec541736ad7e2c75017e2b87123001060
Javier Vera Flores <aczayacatl27@hotmail.com>
2025-02-10 22:07:46 -0600

    reorganizando_carpetas_proyecto

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+export const Footer = ()=>{
+    //logica del footer
+    return(
+        <>
+            <div>
+                <p>Derechos de autor</p>
+                <p>Consulta nuestro aviso de privasidad</p>
+                <p>Rastrea tu pedido</p>
+            </div>
+        </>
+
+    );
+}
